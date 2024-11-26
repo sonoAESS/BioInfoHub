f9eb78e9cc8361b8f7fecd6d2d7a5a267af52e2a
Antonio Elias <eliasyosoto@gmail.com>
2024-11-26 00:46:18 -0500

    staticfiles_dirs

--- BioProgrammer/settings.py
+++ BioProgrammer/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = '/static/'
 
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
